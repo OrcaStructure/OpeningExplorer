006f0865ce1280b811284e4fe9e8165d5686dd35
OrcaStructure <104505623+OrcaStructure@users.noreply.github.com>
2024-09-08 17:07:16 +1000

    build3

--- openingexplorer/next.config.js
+++ openingexplorer/next.config.js
@@ -1,17 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
 const nextConfig = {
   output: 'standalone',
 
-  // Setting the base path to the full path after the hostname and domain
   basePath: '/OpeningExplorer/openingexplorer/out',
   assetPrefix: '/OpeningExplorer/openingexplorer/out/',
-
-  // Optional settings (uncomment if needed)
-  // trailingSlash: true,
-  // skipTrailingSlashRedirect: true,
-  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
