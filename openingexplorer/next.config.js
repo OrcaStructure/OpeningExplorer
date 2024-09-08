6602abd9d3d73368e9d146848020a427dd7ac399
OrcaStructure <104505623+OrcaStructure@users.noreply.github.com>
2024-09-08 17:15:53 +1000

    build4

--- openingexplorer/next.config.js
+++ openingexplorer/next.config.js
@@ -1,6 +1,10 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
 
+  // Set the base path and asset prefix for GitHub Pages deployment
   basePath: '/OpeningExplorer/openingexplorer/out',
   assetPrefix: '/OpeningExplorer/openingexplorer/out/',
 };
